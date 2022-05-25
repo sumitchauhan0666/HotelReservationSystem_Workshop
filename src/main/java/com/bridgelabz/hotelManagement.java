637431cf83ceeaf34e57d0b86bb0e4ac93d4c175
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-25 21:46:51 +0530

    UC1 ability to add Hotel

--- src/main/java/com/bridgelabz/hotelManagement.java
+++ src/main/java/com/bridgelabz/hotelManagement.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class hotemManagement {
+public class hotelManagement {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Management System");
